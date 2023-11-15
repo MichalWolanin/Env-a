b2caa5a817b90990f49b9b510178f7802b6dde23
michalwolanin <michalwolanin02@gmail.com>
2023-11-15 19:01:18 +0100

    Update node

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  '.apps/frontend/src/**/*.{html,js,scss}',
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
