f2df357ceb877d4f205c4df3635e581e948243aa
michalwolanin <michalwolanin02@gmail.com>
2023-11-17 22:10:13 +0100

    Visual changes.

--- apps/frontend/src/app/components/comment/comment.component.ts
+++ apps/frontend/src/app/components/comment/comment.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateCommentComponent } from "../create-comment/create-comment.component";
 import { Comment } from '../../interfaces/comment.interface';
+import { CommentService } from '../../services/comment.service';
 
 @Component({
     selector: 'env-a-comment',
@@ -12,17 +13,28 @@ import { Comment } from '../../interfaces/comment.interface';
 })
 export class CommentComponent {
   @Input() comment!: Comment;
-  isExpanded = false;
-  isReplying = false;
+  isExpanded = signal(false);
+  isReplying = signal(false);
+  commentService = inject(CommentService);
+  nestedComments = signal<Comment[]>([]);
+
+  nestedCommentsEffect = effect(() => {
+    if (this.isExpanded()) {
+      this.commentService.getComments(this.comment._id)
+      .subscribe(comments => {
+        this.nestedComments.set(comments);
+      })
+    };
+  });
 
   toggleReplying() {
-    this.isReplying = !this.isReplying;
-    if (this.isReplying) {
-      this.isExpanded = true;
+    this.isReplying.set(!this.isReplying());
+    if (this.isReplying()) {
+      this.isExpanded.set(true);
     }
   }
 
   toggleExpanded() {
-    this.isExpanded = !this.isExpanded;
+    this.isExpanded.set(!this.isExpanded());
   }
 }
